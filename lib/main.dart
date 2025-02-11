2e706c2d13e778ce3a4d1c4fc884c38791b8139d
KamKnez <kamilo.knezevic@edu.fh-joanneum.at>
2025-02-11 14:34:57 +0100

    final-push

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'my_app.dart';
 import 'my_app_state.dart';
+import 'package:flutter/semantics.dart';
 
 void main() {
   runApp(
@@ -10,4 +11,5 @@ void main() {
       child: MyApp(),
     ),
   );
+  SemanticsBinding.instance.ensureSemantics();
 }
